2acb16a733c243a3af22dae86ae1fc3109275093
yuri <930502900@qq.com>
2018-03-10 12:32:31 +0800

    20180310 12:32

---
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.demo.web;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/getinfo")
+    public String getinfo() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("message", "success");
+        map.put("code", "1");
+        String mapJson = JSON.toJSONString(map);
+        return mapJson;
+    }
+}
