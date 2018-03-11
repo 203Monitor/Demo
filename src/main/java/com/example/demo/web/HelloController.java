378bedf569ef8d1483c728b3f952291bf58915c6
203Monitor <203Monitor@users.noreply.github.com>
2018-03-11 20:09:47 +0800

    change version

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("message", "success");
         map.put("code", "1");
-        map.put("version", "0.0.1.003");
+        map.put("version", "0.0.1.004");
         String mapJson = JSON.toJSONString(map);
         return mapJson;
     }
